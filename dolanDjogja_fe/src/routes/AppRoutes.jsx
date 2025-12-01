5034263c0efbba49196da4e60e5efd0eb3f3358b
Poeww <230712423@students.uajy.ac.id>
2025-12-01 18:32:46 +0700

    update: app routes

--- dolanDjogja_fe/src/routes/AppRoutes.jsx
+++ dolanDjogja_fe/src/routes/AppRoutes.jsx
@@ -1,17 +1,64 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 import Login from "../home/login";
 import Register from "../home/register";
 import Home from "../home/home";
 
+import PaketList from "../pages/admin/paket/paketList";
+import PaketCreate from "../pages/admin/paket/paketCreate";
+import PaketEdit from "../pages/admin/paket/paketEdit";
+
+import { getUser } from "../services/authService";
+
+const PrivateRoute = ({ children, role }) => {
+  const user = getUser();
+
+  if (!user) {
+    return <Navigate to="/login" />;
+  }
+
+  if (role && user.role !== role) {
+    return <Navigate to="/" />;
+  }
+
+  return children;
+};
+
 export default function AppRoutes() {
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/login" element={<Login />} />
-                <Route path="/register" element={<Register />} />
-            </Routes>
-        </BrowserRouter>
-    );
+  return (
+    <BrowserRouter>
+      <Routes>
+        {/* PUBLIC */}
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+
+        {/* ADMIN PAKET */}
+        <Route
+          path="/admin/paket"
+          element={
+            <PrivateRoute role="admin">
+              <PaketList />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/admin/paket/create"
+          element={
+            <PrivateRoute role="admin">
+              <PaketCreate />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/admin/paket/edit/:id"
+          element={
+            <PrivateRoute role="admin">
+              <PaketEdit />
+            </PrivateRoute>
+          }
+        />
+      </Routes>
+    </BrowserRouter>
+  );
 }
