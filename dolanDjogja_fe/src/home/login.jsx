9b611f2dba959f3d9f2ae5f3472ff09223caeb4f
Poeww <230712423@students.uajy.ac.id>
2025-12-01 18:16:17 +0700

    create: login file jsx

---
+++ dolanDjogja_fe/src/home/login.jsx
@@ -0,0 +1,68 @@
+import { useState } from "react";
+import { login } from "../services/authService";
+import { useNavigate } from "react-router-dom";
+
+export default function Login() {
+    const navigate = useNavigate();
+
+    const [form, setForm] = useState({
+        email: "",
+        password: "",
+    });
+
+    const [error, setError] = useState("");
+
+    const handleChange = (e) => {
+        setForm({
+            ...form,
+            [e.target.name]: e.target.value,
+        });
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        setError("");
+
+        try {
+            const res = await login(form);
+
+            if (res.user.role === "admin") {
+                navigate("/admin");
+            } else {
+                navigate("/");
+            }
+        } catch (err) {
+            setError("Email atau password salah.");
+        }
+    };
+
+    return (
+        <div style={{ padding: "40px" }}>
+            <h2>Login</h2>
+
+            {error && <p style={{ color: "red" }}>{error}</p>}
+
+            <form onSubmit={handleSubmit}>
+                <input
+                    type="email"
+                    name="email"
+                    placeholder="Email"
+                    value={form.email}
+                    onChange={handleChange}
+                />
+                <br />
+
+                <input
+                    type="password"
+                    name="password"
+                    placeholder="Password"
+                    value={form.password}
+                    onChange={handleChange}
+                />
+                <br />
+
+                <button type="submit">Login</button>
+            </form>
+        </div>
+    );
+}
