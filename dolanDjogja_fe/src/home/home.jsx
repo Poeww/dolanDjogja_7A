f7db0e63bb8ed83587ad302b81c5f6fcd8b0c61b
Poeww <230712423@students.uajy.ac.id>
2025-12-01 18:24:55 +0700

    create: tes home

---
+++ dolanDjogja_fe/src/home/home.jsx
@@ -0,0 +1,8 @@
+export default function Home() {
+    return (
+        <div style={{ padding: "40px" }}>
+            <h1>Selamat Datang di dolanDjogja!</h1>
+            <p>Ini halaman Home.</p>
+        </div>
+    );
+}
