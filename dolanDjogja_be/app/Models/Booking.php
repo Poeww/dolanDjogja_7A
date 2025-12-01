a3cda24a0f6f89136ea9fe46f95801bc2d4e4728
Poeww <230712423@students.uajy.ac.id>
2025-12-01 17:18:24 +0700

    add: model booking

---
+++ dolanDjogja_be/app/Models/Booking.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Booking extends Model
+{
+    use HasFactory;
+
+    protected $table = 'bookings';
+
+    protected $fillable = [
+        'user_id',
+        'jadwal_trip_id',
+        'jumlah_orang',
+        'total_harga',
+        'metode_pembayaran',
+        'status_pembayaran'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function jadwalTrip()
+    {
+        return $this->belongsTo(JadwalTrip::class, 'jadwal_trip_id');
+    }
+
+    public function payment()
+    {
+        return $this->hasOne(Payment::class, 'booking_id');
+    }
+}
