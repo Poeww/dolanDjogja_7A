fb8d4443b1f3a4f360c71242ea1b25e69c581ab5
Poeww <230712423@students.uajy.ac.id>
2025-12-01 18:56:41 +0700

    add: booking service

---
+++ dolanDjogja_fe/src/services/bookingService.js
@@ -0,0 +1,16 @@
+import API from "./api";
+
+export const createBooking = async (data) => {
+  const res = await API.post("/bookings", data);
+  return res.data;
+};
+
+export const getBookingById = async (id) => {
+  const res = await API.get(`/bookings/${id}`);
+  return res.data;
+};
+
+export const getMyBookings = async () => {
+  const res = await API.get("/my-bookings");
+  return res.data;
+};
