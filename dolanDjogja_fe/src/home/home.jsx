ddcdbe551c59e226613966336e94f3b8c24e6a8b
Poeww <230712423@students.uajy.ac.id>
2025-12-02 01:34:59 +0700

    update: home

--- dolanDjogja_fe/src/home/home.jsx
+++ dolanDjogja_fe/src/home/home.jsx
@@ -1,8 +1,26 @@
+import { Link } from "react-router-dom";
+
 export default function Home() {
-    return (
-        <div style={{ padding: "40px" }}>
-            <h1>Selamat Datang di dolanDjogja!</h1>
-            <p>Ini halaman Home.</p>
-        </div>
-    );
+  return (
+    <div style={{ padding: "40px" }}>
+      <h1>Selamat Datang di dolanDjogja!</h1>
+      <p>Ini halaman Home.</p>
+
+      <Link to="/login">
+        <button
+          style={{
+            padding: "10px 20px",
+            marginTop: "20px",
+            backgroundColor: "#007bff",
+            color: "#fff",
+            border: "none",
+            borderRadius: "5px",
+            cursor: "pointer"
+          }}
+        >
+          Login
+        </button>
+      </Link>
+    </div>
+  );
 }
