6281187b1b56120bee5886369fa9ce83dcc03c8a
Poeww <230712423@students.uajy.ac.id>
2025-12-01 17:15:32 +0700

    add: model paket wisata

---
+++ dolanDjogja_be/app/Models/PaketWisata.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class PaketWisata extends Model
+{
+    use HasFactory;
+
+    protected $table = 'paket_wisata';
+
+    protected $fillable = [
+        'nama_paket',
+        'deskripsi',
+        'harga',
+        'durasi',
+        'lokasi_tujuan',
+        'kuota',
+        'gambar_thumbnail'
+    ];
+
+    public function destinasi()
+    {
+        return $this->belongsToMany(Destinasi::class, 'paket_destinasi', 'paket_id', 'destinasi_id');
+    }
+
+    public function jadwal()
+    {
+        return $this->hasMany(JadwalTrip::class, 'paket_id');
+    }
+}
